fa9fd02c99463f59caebccb459b315265ca64246
Moritz Lutz <moritz.lutz@gmail.com>
2020-10-13 18:22:59 +0200

    changed colors of covenants to match the official colors

--- js/internal/chart/helper/ChartColorHelper.js
+++ js/internal/chart/helper/ChartColorHelper.js
@@ -1,19 +1,19 @@
 var covenantChoiceColor = {
-    kyrian_min: "#0000cc",
-    kyrian_max: "#8080ff",
-    night_fae_min: "#a52a2a",
-    night_fae_max: "#da7171",
-    necrolord_min: "#e5d334",
-    necrolord_max: "#f0e68c",
-    venthyr_min: "#008b8b",
-    venthyr_max: "#33ffff",
+    kyrian_min: "#0033CC",
+    kyrian_max: "#0099FF",
+    night_fae_min: "#9933CC",
+    night_fae_max: "#9966CC",
+    necrolord_min: "#009933",
+    necrolord_max: "#00CC66",
+    venthyr_min: "#CC0000",
+    venthyr_max: "#FF3300",
 }
 
 var covenantColor = {
-    kyrian: "#0000cc",
-    night_fae: "#a52a2a",
-    necrolord: "#e5d334",
-    venthyr: "#008b8b",
+    kyrian: "#0033CC",
+    night_fae: "#9933CC",
+    necrolord: "#009933",
+    venthyr: "#CC0000",
 }
 
 var getCovenantChoiceColor = function (key) {
