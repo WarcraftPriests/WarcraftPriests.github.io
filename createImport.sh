8a9770ebede2963e0a312ed9c36073ad446ced23
Moritz Lutz <Moritz.Lutz@gmail.com>
2020-12-05 19:16:31 +0100

    update

--- createImport.sh
+++ createImport.sh
@@ -1,40 +1,43 @@
 #!/bin/sh
 
-rm js/internal/chart/ChartImport.js
 
 ## Adds helper
-cat js/internal/helper/Converter.js >> js/internal/chart/ChartImport.js
+cat js/internal/helper/Converter.js > js/chart_import.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
-cat js/internal/helper/Constants.js >> js/internal/chart/ChartImport.js
+cat js/internal/helper/Constants.js >> js/chart_import.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 
 ## Adds Chart
-cat js/internal/chart/Chart.js >> js/internal/chart/ChartImport.js
+cat js/internal/chart/Chart.js >> js/chart_import.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 
 ## Adds chart helper
-cat js/internal/chart/helper/ColorHelper.js >> js/internal/chart/ChartImport.js
+cat js/internal/chart/helper/ColorHelper.js >> js/chart_import.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
-cat js/internal/chart/helper/DataHelper.js >> js/internal/chart/ChartImport.js
+cat js/internal/chart/helper/DataHelper.js >> js/chart_import.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
-cat js/internal/chart/helper/WowheadHelper.js >> js/internal/chart/ChartImport.js
+cat js/internal/chart/helper/WowheadHelper.js >> js/chart_import.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 
 ## Adds chart defintions
-cat js/internal/chart/definitions/Definitions.js >> js/internal/chart/ChartImport.js
+cat js/internal/chart/definitions/Definitions.js >> js/chart_import.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 
-cat js/yaml/yaml.min.js >> js/internal/chart/ChartImport.js
+cat js/internal/csv/Csv.js >> js/chart_import.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 
-cat js/internal/helper/ChartImportBasic.js >> js/internal/chart/ChartImport.js
+cat js/yaml/yaml.min.js >> js/chart_import.js
+sed -i -e '$a\' js/internal/chart/ChartImport.js
+sed -i -e '$a\' js/internal/chart/ChartImport.js
+
+cat js/internal/helper/ChartImportBasic.js >> js/chart_import.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
 sed -i -e '$a\' js/internal/chart/ChartImport.js
