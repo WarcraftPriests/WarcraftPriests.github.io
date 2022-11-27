a1cf114eb5fd7e391225c8a4242ad4c977ecb77a
Moritz Lutz <114048565+vethyn@users.noreply.github.com>
2022-11-27 20:53:36 +0100

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -1,3 +1,10 @@
+jQuery.get({url: baseUrl + "/internal/talents.yml", dataType: "text"})
+        .done(function (data) {
+            talentData = jsyaml.load(data);
+            //initializeButtons();
+        }
+);
+
 jQuery.get({url: baseUrl + "/config.yml", dataType: "text"})
         .done(function (data) {
             configData = jsyaml.load(data);
