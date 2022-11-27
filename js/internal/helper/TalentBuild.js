069208a0cd4020368618b04d510e5d93a865177b
Moritz Lutz <114048565+vethyn@users.noreply.github.com>
2022-11-27 09:03:51 +0100

    talent build: initial implementation (#52)

---
+++ js/internal/helper/TalentBuild.js
@@ -0,0 +1,24 @@
+$(".talentBuild").click(function () {
+    $header = $(this);
+    $content = $header.next();
+    //$content = $temp.next();
+    $content.slideToggle(500, function () {
+        $header.text(function () {
+            return $content.is(":visible") ? "Hide Talent Build" : "Show Talent Build";
+        });
+    });
+});
+
+function replaceTalentId(currTalent, currFightStyle) {
+    let selectedFightStyle = undefined;
+    if(currFightStyle == 'twotarget') {
+        selectedFightStyle = '2t';
+    } else if(currFightStyle == 'fourtarget') {
+        selectedFightStyle = '4t';
+    } else {
+        selectedFightStyle = currFightStyle.toLowerCase();
+    }
+
+    document.getElementById(talentBuildIdDiv).innerHTML = document.getElementById(talentBuildIdDiv).innerHTML.replaceAll('%talentId%',configData['builds'][currTalent.replaceAll('_', '-')][selectedFightStyle])
+    document.getElementById(talentBuildContentDiv).innerHTML = document.getElementById(talentBuildContentDiv).innerHTML.replaceAll('%talentId%',configData['builds'][currTalent.replaceAll('_', '-')][selectedFightStyle])
+}   
